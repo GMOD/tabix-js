9aed0222edba5ca1eaf54a04d36f76146cdcdb12
Robert Buels <rbuels@gmail.com>
2018-08-28 15:57:48 -0700

    test using CSI with a tabix file

--- test/tabixindexedfile.test.js
+++ test/tabixindexedfile.test.js
@@ -104,4 +104,42 @@ describe('tabix file', () => {
     await f.getLines('ctgA', 4369, 4370, lines.push.bind(lines))
     expect(lines.length).toEqual(1)
   })
+
+  it('can query test.vcf.gz with a CSI index', async () => {
+    const f = new TabixIndexedFile({
+      path: require.resolve('./data/test.vcf.gz'),
+      csiPath: require.resolve('./data/test.vcf.gz.csi'),
+    })
+
+    const lines = []
+    await f.getLines('ctgB', 0, Infinity, l => lines.push(l))
+    expect(lines.length).toEqual(0)
+
+    await f.getLines('ctgA', -2, 3000, lines.push.bind(lines))
+    expect(lines.length).toEqual(0)
+    await f.getLines('ctgA', -50, -20, lines.push.bind(lines))
+    expect(lines.length).toEqual(0)
+    await f.getLines('1', 4000, 5000, lines.push.bind(lines))
+    expect(lines.length).toEqual(0)
+    lines.length = 0
+    await f.getLines('1', 1206810423, 1206810423, lines.push.bind(lines))
+    expect(lines.length).toEqual(0)
+    lines.length = 0
+    await f.getLines('1', 1206810422, 1206810423, lines.push.bind(lines))
+    expect(lines.length).toEqual(1)
+    expect(lines[0]).toEqual(
+      '1	1206810423	.	T	A	25	.	DP=19;VDB=0.0404;AF1=0.5;AC1=1;DP4=3,7,3,6;MQ=37;FQ=28;PV4=1,1,1,0.27	GT:PL:GQ	0/1:55,0,73:58',
+    )
+    lines.length = 0
+    await f.getLines('1', 1206810423, 1206810424, lines.push.bind(lines))
+    expect(lines.length).toEqual(0)
+    await f.getLines('1', 1206810423, 1206849288, lines.push.bind(lines))
+    expect(lines.length).toEqual(36)
+    expect(lines[35]).toEqual(
+      '1	1206849288	.	G	A	106	.	DP=23;VDB=0.0399;AF1=1;AC1=2;DP4=0,0,16,7;MQ=35;FQ=-96	GT:PL:GQ	1/1:139,69,0:99',
+    )
+    lines.length = 0
+    await f.getLines('1', 1206810423, 1206810424, lines.push.bind(lines))
+    expect(lines.length).toEqual(0)
+  })
 })
