d97a5a7de9f8d003dcd9a459908498143fb830b1
Colin <colin.diesh@gmail.com>
2019-10-08 17:03:13 -0400

    Add tests that ensure fileoffset consistency

--- src/unzip.js
+++ src/unzip.js
@@ -1,6 +1,6 @@
-const { Inflate, Z_SYNC_FLUSH } = require('pako')
+import { Inflate, Z_SYNC_FLUSH } from 'pako'
 
-function pakoUnzip(inputData) {
+export function unzip(inputData) {
   let strm
   let pos = 0
   let i = 0
@@ -26,20 +26,25 @@ function pakoUnzip(inputData) {
 // similar to pakounzip, except it does extra counting and
 // trimming to make sure to return only exactly the data
 // range specified in the chunk
-function unzipChunk(inputData, chunk) {
+export function unzipChunk(inputData, chunk) {
   let strm
-  let pos = 0
+  let cpos = 0
+  let dpos = 0
   const decompressedBlocks = []
+  const cpositions = []
+  const dpositions = []
   let inflator
-  const fileStartingOffset = chunk.minv.blockPosition
-  for (;;) {
-    const remainingInput = inputData.slice(pos)
+  do {
+    const remainingInput = inputData.slice(cpos)
     inflator = new Inflate()
     ;({ strm } = inflator)
     inflator.push(remainingInput, Z_SYNC_FLUSH)
     if (inflator.err) throw new Error(inflator.msg)
 
-    decompressedBlocks.push(Buffer.from(inflator.result))
+    const buffer = Buffer.from(inflator.result)
+    decompressedBlocks.push(buffer)
+    cpositions.push(cpos)
+    dpositions.push(dpos)
 
     if (decompressedBlocks.length === 1 && chunk.minv.dataPosition) {
       // this is the first chunk, trim it
@@ -47,34 +52,25 @@ function unzipChunk(inputData, chunk) {
         chunk.minv.dataPosition,
       )
     }
-    if (fileStartingOffset + pos >= chunk.maxv.blockPosition) {
+    if (chunk.minv.blockPosition + cpos >= chunk.maxv.blockPosition) {
       // this is the last chunk, trim it and stop decompressing
+      // note if it is the same block is minv it subtracts that already
+      // trimmed part of the slice length
+
       decompressedBlocks[decompressedBlocks.length - 1] = decompressedBlocks[
         decompressedBlocks.length - 1
       ].slice(
         0,
-        chunk.minv.blockPosition === chunk.maxv.blockPosition
+        chunk.maxv.blockPosition === chunk.minv.blockPosition
           ? chunk.maxv.dataPosition - chunk.minv.dataPosition + 1
           : chunk.maxv.dataPosition + 1,
       )
       break
     }
-    // if we are not at the last chunk and there is no more input available,
-    // something is wrong.
-    if (!strm.avail_in) {
-      throw new Error(
-        `unexpected end of input decompressing chunk ${chunk.toString()}`,
-      )
-    }
+    cpos += strm.next_in
+    dpos += buffer.length
+  } while (strm.avail_in)
 
-    pos += strm.next_in
-  }
-
-  const result = Buffer.concat(decompressedBlocks)
-  return result
-}
-
-module.exports = {
-  unzip: pakoUnzip,
-  unzipChunk,
+  const buffer = Buffer.concat(decompressedBlocks)
+  return { buffer, cpositions, dpositions }
 }
