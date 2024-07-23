fa6151b1a3f98e35f1eab575f99f6292d489d793
Colin <colin.diesh@gmail.com>
2024-07-22 20:24:11 -0400

    Change abortable-promise-cache to @gmod/abortable-promise-cache

--- src/tabixIndexedFile.ts
+++ src/tabixIndexedFile.ts
@@ -1,4 +1,4 @@
-import AbortablePromiseCache from 'abortable-promise-cache'
+import AbortablePromiseCache from '@gmod/abortable-promise-cache'
 import LRU from 'quick-lru'
 import { Buffer } from 'buffer'
 import { GenericFilehandle, LocalFile } from 'generic-filehandle'
