4c85e2f35574005500592e8eac3054effe657782
Colin <colin.diesh@gmail.com>
2026-03-28 17:10:10 -0400

    Export VirtualOffset type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as TabixIndexedFile } from './tabixIndexedFile.ts'
 export { default as CSI } from './csi.ts'
 export { default as TBI } from './tbi.ts'
+export { default as VirtualOffset } from './virtualOffset.ts'
